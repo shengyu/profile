e316256cc9c9203652118f30100b52451974d15b
Shengyu Liu <usungu@gmail.com>
2022-01-10 02:08:54 +0800

    Create website with nextjs

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Shengyu</title>
+        <meta name="description" content="Shengyu's website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <div>Hi, welcome!</div>
+      </main>
+
+      <footer></footer>
+    </div>
+  );
+}
